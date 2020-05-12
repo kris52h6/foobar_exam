8de7d5a9faf639ba0d0d629b5ad2f5d67d148308
Kristian <kris52h6@stud.kea.dk>
2020-05-12 14:37:52 +0200

    first

---
+++ server.js
@@ -0,0 +1,104 @@
+/* eslint-env node, es6 */
+const express = require("express");
+const bodyParser = require("body-parser");
+var app = express();
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST"); //OPTIONS
+  next();
+});
+
+const { FooBar } = require("./src/foobar");
+const { Customer } = require("./src/customer");
+const { Order } = require("./src/order");
+const { Beer } = require("./src/beer");
+const beers = [
+  "El Hefe",
+  "Fairy Tale Ale",
+  "GitHop",
+  "Hollaback Lager",
+  "Hoppily Ever After",
+  "Mowintime",
+  "Row 26",
+  "Ruined Childhood",
+  "Sleighride",
+  "Steampunk",
+]; //TODO: should be taken from conf
+/*
+app.get("/", function (req, res) {
+  res.json({
+    message:
+      "Nothing here, please read the documentation or try something like GET /beertypes, GET /data/:key or POST /order/:key",
+  });
+});*/
+app.get("/", function (req, res) {
+  // send back a json response
+  let data = FooBar.getData();
+  delete data.beertypes;
+  res.json(data);
+});
+app.get("/beertypes", function (req, res) {
+  let data = FooBar.getData();
+  res.json(data.beertypes);
+});
+
+app.post("/order", function (req, res) {
+  //const key = req.params.key;
+  const structure = req.body;
+  if (!Array.isArray(structure)) {
+    res.send({ message: "Wrong data format supplied", status: 500 });
+  }
+  console.log(structure);
+  /*const structure = [
+    { name: "Hoppily Ever After", amount: 1 },
+    { name: "Hoppily Ever After", amount: 1 },
+    { name: "Hoppily Ever After", amount: 1 },
+    { name: "Hoppily Ever After", amount: 1 },
+  ];*/
+
+  //Validate data structure
+  const hasProps = (currentItem) => {
+    return currentItem.name && currentItem.amount;
+  };
+
+  if (!structure.every(hasProps)) {
+    res.send({
+      message: "Wrong data format supplied, missing name or amount",
+      status: 500,
+    });
+  }
+
+  structure.forEach((item) => {
+    if (!beers.includes(item.name)) {
+      res.send({
+        message: "Unknown beer: " + item.name,
+        status: 500,
+      });
+    }
+  });
+  // expected output: true
+  const customer = new Customer();
+  const order = new Order(customer);
+
+  const beerTypes = FooBar.getAvailableBeerTypes();
+  for (let i = 0; i < structure.length; i++) {
+    const beerData = beerTypes.find((b) => b.name === structure[i].name);
+    for (let amount = 0; amount < structure[i].amount; amount++) {
+      const beer = new Beer(beerData);
+      order.addBeer(beer);
+    }
+  }
+  FooBar.addCustomer(customer);
+
+  // res.send converts to json as well
+  // but req.json will convert things like null and undefined to json too although its not valid
+  res.send({ message: "added", status: 200 });
+});
+
+app.listen(process.env.PORT || 3000);
---
+++ src/foobar.js
@@ -0,0 +1,26 @@
+//====================
+const version = "0.07";
+//====================
+
+const { Bar } = require("./bar.js");
+const { CustomerGenerator } = require("./customergenerator");
+const jsonConf = require("./configuration.json");
+function createBar(name) {
+  const bar = new Bar(name);
+  bar.version = version;
+
+  bar.loadConfiguration(jsonConf);
+  console.log("Created Bar '" + bar.name + "' - ready for customers ...");
+  const customerGenerator = new CustomerGenerator(bar);
+  customerGenerator.start();
+  // calling bar.open, will open it, when the configuration is complete - then the .whenOpen callback will be called
+  bar.open();
+
+  // return the bar
+  return bar;
+}
+
+const bar = createBar("FooBar");
+module.exports = {
+  FooBar: bar,
+};
