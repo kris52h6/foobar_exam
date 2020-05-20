af0d8cf07df9370f6dc50223d6d027e9789f11c7
Victor Øbro <vict889f@stud.kea.dk>
2020-05-20 12:19:23 +0200

    yeet

--- test.js
+++ test.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", start);
 
-let costumerQueue = [];
+const costumerQueue = [];
+costumerQueue.length = 8;
+
+let myQueue;
 
 function start() {
   getJson();
@@ -14,6 +17,29 @@ async function getJson(myJson) {
   console.log(myJson);
   console.log(myJson.queue.length);
 
-  document.querySelector("h1").textContent = "Costumers in queue:" + " " + myJson.queue.length;
-  // prepData(myJson);
+  // document.querySelector("h1").textContent = "Costumers in queue:" + " " + myJson.queue.length;
+
+  setHeights(myJson);
+}
+
+function count(myJson) {
+  let height = myJson.queue.length;
+
+  costumerQueue.push(height);
+
+  costumerQueue.shift();
+
+  setTimeout(getJson, 10000);
+}
+
+function setHeights(myJson) {
+  console.log(myJson);
+
+  for (let i = 0; i < costumerQueue.length; i++) {
+    let bar = document.querySelector(`#barParent > div:nth-child(${i + 1})`);
+
+    bar.style.setProperty("--height", costumerQueue[i]);
+  }
+
+  count(myJson);
 }
