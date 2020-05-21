c46ede9f56a47344829082a35de72db0814268c0
Kristian <kris52h6@stud.kea.dk>
2020-05-21 15:50:31 +0200

    dashboard changes, form local storage start

--- form.js
+++ form.js
@@ -1,14 +1,18 @@
 document.addEventListener("DOMContentLoaded", init);
 
 const beerURL = "https://kristian-victor-foobar.herokuapp.com/beertypes";
+let cartArray = [];
+let cartlist = document.querySelector(".cartlist");
 
 function init() {
-  console.log("hep");
   getBeers();
+  if (cartlist) {
+    displayCart();
+  }
 }
 
 async function getBeers() {
-  let jsonData = await fetch("https://kristian-victor-foobar.herokuapp.com/beertypes");
+  let jsonData = await fetch(beerURL);
   minJson = await jsonData.json();
   displayBeers(minJson);
 }
@@ -17,7 +21,8 @@ function displayBeers(minJson) {
   const modtagerKloner = document.querySelector(".beerlist");
   const skabelon = document.querySelector("template");
   modtagerKloner.innerHTML = "";
-  console.log(minJson);
+  let taeller = 0;
+  // console.log(minJson);
 
   if (modtagerKloner) {
     modtagerKloner.innerHTML = "";
@@ -26,13 +31,54 @@ function displayBeers(minJson) {
       const klon = skabelon.cloneNode(true).content;
       klon.querySelector(".name").textContent = beertype.name;
       // klon.querySelector(".price").textContent = beer.price + "DKK";
-
-      // beertype.photo = "images/" + beertype.name + ".png";
-      // klon.querySelector("img").src = beertype.photo;
+      klon.querySelector("button").id = taeller;
+      klon.querySelector("button").addEventListener("click", (event) => {
+        beerCount(beertype, event);
+      });
+      taeller++;
 
       klon.querySelector("img").src = "images/" + beertype.label;
       // klon.querySelector("img").alt = beertype.label;
       modtagerKloner.appendChild(klon);
+      // modtagerKloner.querySelector("button").addEventListener("click", () => {
+      //   beerCount(beertype, this);
+      // });
     });
   }
+}
+
+function beerCount(beertype, event) {
+  let beerAmount = document.querySelectorAll(".quantity")[event.target.id].value;
+  console.log(beerAmount);
+
+  let beerObject = {
+    name: beertype.name,
+    amount: beerAmount,
+  };
+
+  cartArray.push(beerObject);
+  console.log(cartArray);
+
+  localStorage.setItem("order", JSON.stringify(beerObject));
+}
+
+function displayCart() {
+  const skabelon = document.querySelector("template");
+  cartlist.innerHTML = "";
+  orderArray = [];
+
+  let currentOrder = localStorage.getItem("order");
+
+  let orderParse = JSON.parse(currentOrder);
+  console.log(orderParse);
+
+  orderArray.push(orderParse);
+
+  console.log(orderArray);
+  orderArray.forEach((order) => {
+    const klon = skabelon.cloneNode(true).content;
+    klon.querySelector("h1").textContent = order.name;
+    klon.querySelector("h2").textContent = order.amount;
+    cartlist.appendChild(klon);
+  });
 }
