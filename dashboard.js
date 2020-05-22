caf9d969e4f9cd16e28119f696ee542c7321e3d0
Victor Øbro <vict889f@stud.kea.dk>
2020-05-22 14:01:08 +0200

    modes

--- dashboard.js
+++ dashboard.js
@@ -23,6 +23,17 @@ async function getJson(myJson) {
   // setHeights(myJson);
 }
 
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.querySelector("body").setAttribute("data-theme", "dark");
+  } else {
+    document.querySelector("body").setAttribute("data-theme", "light");
+  }
+}
+toggleSwitch.addEventListener("change", switchTheme, false);
+
 // function count(myJson) {
 //   let height = myJson.queue.length;
 
@@ -66,35 +77,36 @@ function bartenderStatus(myJson) {
       }
 
       //   klon.querySelector("h3").textContent = bartender.status;
+      let tema = document.querySelector("body").dataset.theme;
 
       if (bartender.statusDetail === "pourBeer") {
         klon.querySelector("h4").textContent = "Pouring Beer";
-        klon.querySelector(".icons>.smallIcon>.icon").src = "icons/pouringBeer.png";
+        klon.querySelector(".icons>.smallIcon>.icon").src = "icons/" + tema + "/pouringBeer.png";
       }
       if (bartender.statusDetail === "receivePayment") {
         klon.querySelector("h4").textContent = "Recieving Payment";
-        klon.querySelector(".icons>.smallIcon>.icon").src = "icons/recivingPayment.png";
+        klon.querySelector(".icons>.smallIcon>.icon").src = "icons/" + tema + "/recivingPayment.png";
       }
       if (bartender.statusDetail === "replaceKeg") {
         klon.querySelector("h4").textContent = "Replacing Keg";
-        klon.querySelector(".icons>.smallIcon>.icon").src = "icons/changingKeg.png";
+        klon.querySelector(".icons>.smallIcon>.icon").src = "icons/" + tema + "/changingKeg.png";
       }
       if (bartender.statusDetail === "reserveTap") {
         klon.querySelector("h4").textContent = "Reserving Tap";
-        klon.querySelector(".icons>.smallIcon>.icon").src = "icons/reserveTap.png";
+        klon.querySelector(".icons>.smallIcon>.icon").src = "icons/" + tema + "/reserveTap.png";
       }
       if (bartender.statusDetail === "releaseTap") {
         klon.querySelector("h4").textContent = "Releasing Tap";
-        klon.querySelector(".icons>.smallIcon>.icon").src = "icons/releasingTap.png";
+        klon.querySelector(".icons>.smallIcon>.icon").src = "icons/" + tema + "/releasingTap.png";
       }
       if (bartender.statusDetail === "startServing") {
         klon.querySelector("h4").textContent = "Starting Service";
-        klon.querySelector(".icons>.smallIcon>.icon").src = "icons/startingService.png";
+        klon.querySelector(".icons>.smallIcon>.icon").src = "icons/" + tema + "/startingService.png";
       }
 
       if (bartender.statusDetail === "waiting") {
         klon.querySelector("h4").textContent = "Waiting";
-        klon.querySelector(".icons>.smallIcon>.icon").src = "icons/waiting.png";
+        klon.querySelector(".icons>.smallIcon>.icon").src = "icons/" + tema + "/waiting.png";
       }
 
       //   klon.querySelector("h4").textContent = bartender.statusDetail;
